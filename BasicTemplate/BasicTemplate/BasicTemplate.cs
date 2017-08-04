a112e62c5e462f093d41f1821a729e73ac371353
Herb9 <pksxtm@gmail.com>
2017-08-04 23:51:24 +0900

    Initial commit

---
+++ BasicTemplate/BasicTemplate/BasicTemplate.cs
@@ -0,0 +1,34 @@
+﻿
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+
+/*
+     ________  ___  _______   ________     
+    |\   __  \|\  \|\  ___ \ |\   __  \    
+    \ \  \|\  \ \  \ \   __/|\ \  \|\  \   
+     \ \   ____\ \  \ \  \_|/_\ \   __  \  
+      \ \  \___|\ \  \ \  \_|\ \ \  \ \  \ 
+       \ \__\    \ \__\ \_______\ \__\ \__\
+        \|__|     \|__|\|_______|\|__|\|__|
+        
+    PIEA, MiNET Developers' Organization.
+*/
+
+namespace BasicTemplate
+{
+
+    [Plugin(PluginName = "BasicTemplate", Description = "PIEA plugin basic template.", PluginVersion = "None", Author = "PIEA")]
+    public class BasicTemplate : Plugin
+    {
+
+        protected override void OnEnable()
+        {
+
+        }
+
+        public override void OnDisable()
+        {
+   
+        }
+    }
+}
